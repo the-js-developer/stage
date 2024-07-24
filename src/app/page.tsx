66957634165e64e8f6f5411cc40396d23f3a609a
the-js-developer <70698005+the-js-developer@users.noreply.github.com>
2024-07-24 18:07:19 +0530

    fix api  url issue

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,45 @@
-import { fetchUsersList } from "src/utils/fetchServerData";
 import UsersWithStoriesList from "src/app/(home)/_components/UsersWithStoriesList";
+import { UserData } from "src/utils/types";
+// this is server component , we can fetch the results here directly
+// instead of calling the api.
 
+const mockUserList: UserData[] = [{
+  id: '1',
+  insta_handle: 'mono_repo',
+  img: 'images/person.svg'
+},
+{
+  id: '2',
+  insta_handle: 'micro_service_http_you',
+  img: 'images/person.svg'
+},
+{
+  id: '3',
+  insta_handle: 'git_hub',
+  img: 'images/person.svg'
+},
+{
+  id: '4',
+  insta_handle: 'telemetry',
+  img: 'images/person.svg'
+},
+{
+  id: '5',
+  insta_handle: 'telemetry',
+  img: 'images/person.svg'
+},
+{
+  id: '6',
+  insta_handle: 'telemetry',
+  img: 'images/person.svg'
+},
+{
+  id: '7',
+  insta_handle: 'telemetry',
+  img: 'images/person.svg'
+}];
 export default async function Home() {
-  const usersList = await fetchUsersList();
+  const usersList = mockUserList;
   return (
     <main className="flex h-screen flex-col items-center justify-between px-4">
       <div className="text-left w-full">
--- src/utils/fetchServerData.ts
+++ src/utils/fetchServerData.ts
@@ -1,18 +1,4 @@
-import { Story, UserData } from "./types";
-
-const API_URL = process.env.API_URL
-export const fetchUsersList = async (): Promise<UserData[]> => {
-  const response = await fetch(`${API_URL}/api/users`, {
-    method: "GET",
-  });
-     if (!response.ok) {
-       // This will activate the closest `error.js` Error Boundary
-       throw new Error('Failed to fetch data')
-     }
-     
-    const jsonResponse = await response.json();
-    return jsonResponse?.data;
-}
+import { Story } from "./types";
 
 export const fetchUserStories = async (userId: string): Promise<Story[]> => {
   const response = await fetch(`api/stories/${userId}`, {
